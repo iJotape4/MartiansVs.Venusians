024d1a535cf208e0bba32fc8bc4a70ca5faa9220
Juan Pablo <jcamacho249@unab.edu.co>
2021-04-13 09:36:03 -0500

    Commit dado

---
+++ Assets/_Scripts/Dado.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dado : MonoBehaviour
+{
+    public Text texto;
+    public cara[] caras;
+    public int NumeroActual;
+    public Vector3 PosInicial;
+
+    public bool moviendo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        NumeroDado();
+        caras = GetComponentsInChildren<cara>();
+        
+        PosInicial = GetComponent<Dado>().transform.position;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsMoving();
+    }
+
+    void NumeroDado()
+    {
+        for (int i =0; i<caras.Length; i++)
+        {
+            if (caras[i].TocaSuelo)
+            {
+                NumeroActual = 7 - caras[i].Numero;
+            }
+        }
+        Invoke("NumeroDado", 0.5f);
+
+    }
+
+    public void TirarDado()
+    {
+
+        transform.position = PosInicial;
+        float FuerzaInicial = Random.Range(-10, 10 );
+        float FuerzaInicial2 = Random.Range(10, 10);
+        float multplier = Random.Range(10, 10);
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().AddForce(new Vector3(FuerzaInicial * multplier, 0, FuerzaInicial2* multplier));
+        GetComponent<Rigidbody>().rotation = Random.rotation;
+    }
+
+    public bool IsMoving()
+    {
+        return  !GetComponent<Rigidbody>().IsSleeping();
+    }
+}
---
+++ Assets/_Scripts/cara.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cara : MonoBehaviour
+{
+
+    public int Numero;
+    public bool TocaSuelo;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        Numero = int.Parse(GetComponent<cara>().name) ;
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Tablero")
+        TocaSuelo = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        TocaSuelo = false;
+    }
+
+}
