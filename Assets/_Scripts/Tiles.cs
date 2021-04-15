1c455ea10e1852ba570e111b78965ae2818241f2
UselessVoid <48776351+UselessVoid@users.noreply.github.com>
2021-04-15 13:13:10 -0500

    Completing Tiles script

--- Assets/_Scripts/Tiles.cs
+++ Assets/_Scripts/Tiles.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Tiles : MonoBehaviour
 {
-  public bool current = false;
-  public bool target = false;
-    // Start is called before the first frame update
+    public bool walkable = true;
+    public bool current = false;
+    public bool target = false;
+    public bool selectable = false;
+    public List<Tiles> adjacencyList = new List<Tiles>();
+    public bool visited = false;
+    public Tiles parent = null;
+    public int distance = 0;
+
     void Start()
     {
         
@@ -18,8 +24,57 @@ public class Tiles : MonoBehaviour
         if (current)
         {
             GetComponent<Renderer>().material.color=Color.magenta;
-        }else if(target){
+        }
+        else if(target)
+        {
             GetComponent<Renderer>().material.color=Color.green;
+        }
+        else if(selectable)
+        {
+            GetComponent<Renderer>().material.color=Color.red;
+        }
+        else
+        {
+            //
+        }
+    }
+
+    public void Reset()
+    {
+        adjacencyList.Clear();
+        current = false;
+        target = false;
+        selectable = false;
+        visited = false;
+        parent = null;
+        distance = 0;
+    }
+
+    public void FindNeighbors(float jumpHeight)
+    {
+        CheckTile(Vector3.forward, jumpHeight);
+        CheckTile(-Vector3.forward, jumpHeight);
+        CheckTile(Vector3.right, jumpHeight);
+        CheckTile(-Vector3.right, jumpHeight);
+    }
+
+    public void CheckTile(Vector3 direction, float jumpHeight)
+    {
+        Vector3 halfExtents = new Vector3(0.25f, (1 + jumpHeight) / 2.0f, 0.25f);
+        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtents);
+
+        foreach (Collider item in colliders)
+        {
+            Tiles tiles = item.GetComponent<Tiles>();
+            if (tiles != null && tiles.walkable)
+            {
+                RaycastHit hit;
+
+                if (!Physics.Raycast(tiles.transform.position, Vector3.up, out hit, 1))
+                {
+                    adjacencyList.Add(tiles);
+                }
+            }
         }
     }
 }
