9f0b585f15b662f06132b27d8fbf3bccc4bc6fea
Yeiduard <Ygonzalez689@unab.edu.co>
2021-04-14 23:20:12 -0500

    control y movimiento del personaje con seguimiento de camara

---
+++ Assets/_Scripts/PlayerCamara.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject jugador;
+    private Vector3  posicionR;
+
+    void Start()
+    {
+        posicionR =  transform.position-jugador.transform.position ;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position=jugador.transform.position+posicionR;
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    public float velocidad;
+
+    public void awake(){
+        //rigidbody=GetComponent<Rigidbody>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+          rigidbody=GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float movH=Input.GetAxis("Horizontal");
+        float movV=Input.GetAxis("Vertical");
+        Vector3 movimiento= new Vector3 (movH,0,movV);
+        rigidbody.AddForce(movimiento*velocidad);
     }
 }
