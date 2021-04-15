c9fe0068376b60dc52a09190dced76998bb4c16c
Yeiduard <Ygonzalez689@unab.edu.co>
2021-04-15 09:42:59 -0500

    Control Camara
    rotacion, cambio de vista

---
+++ Assets/_Scripts/Tiles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+  public bool current = false;
+  public bool target = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current)
+        {
+            GetComponent<Renderer>().material.color=Color.magenta;
+        }else if(target){
+            GetComponent<Renderer>().material.color=Color.green;
+        }
+    }
+}
