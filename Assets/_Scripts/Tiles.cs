013f400a1b27ad65c3acb4c78740ba12a654fbd9
Juan Pablo <jcamacho249@unab.edu.co>
2021-04-25 11:53:40 -0500

    Coloración nice y paso de turnos nice
    >BLockers<
    -Blocker al clickear dentro de un volcan
    -Blocker al colisionar una unidad con otra

--- Assets/_Scripts/Tiles.cs
+++ Assets/_Scripts/Tiles.cs
@@ -6,37 +6,39 @@ using UnityEngine;
 
 public class Tiles : MonoBehaviour
 {
-  
+
 
     public MeshRenderer _meshRenderer;
     public Transform _transform;
     public GameObject[] _tilesList;
     public bool UnitHere;
-   void Start(){
-        _meshRenderer=  GetComponent<MeshRenderer>();
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
         _tilesList = GameManager.Instance.board;
-
+        UnitHere = false;
 
         _transform = GetComponent<Transform>();
-   }
+    }
 
 
     private void Update()
     {
-        
+
     }
-    void colorear(string color){
-    
+    void colorear(string color)
+    {
+
         _meshRenderer.material = (Resources.Load<Material>(color));
 
-}
+    }
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == ("Unit"))
         {
             UnitHere = true;
 
-            if (other.GetComponent<PlayerController>().PlayerTurn == GameManager.Instance.JugadorActual 
+            if (other.GetComponent<PlayerController>().PlayerTurn == GameManager.Instance.JugadorActual
                 && other.GetComponent<NavMeshController>().isclicked == false)
             {
                 CalculateColoration();
@@ -52,7 +54,7 @@ public class Tiles : MonoBehaviour
     {
         for (int i = 0; i < GameManager.Instance.board.Length; i++)
         {
-            
+
             if (Vector3.Distance(_transform.position, _tilesList[i].transform.position) <= Dado.Instance.NumeroActual)
             {
                 if (_tilesList[i].GetComponent<Tiles>().UnitHere && _tilesList[i].GetComponent<Tiles>() != this)
@@ -64,13 +66,21 @@ public class Tiles : MonoBehaviour
             {
                 _tilesList[i].GetComponent<Tiles>().colorear("CasillaBlueSprite");
             }
-        }  
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 10)
         {
             colorear("Tile");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("Unit"))
+        {
+            UnitHere = false;
         }
     }
 }
