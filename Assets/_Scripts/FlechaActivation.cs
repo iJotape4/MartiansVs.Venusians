920f14431808dcc68b7ce66218e6a2b992fb6fa2
Juan Pablo <jcamacho249@unab.edu.co>
2021-04-28 01:31:14 -0500

    Flechas de apuntado para seleccion de personajes :arrow_down:

---
+++ Assets/_Scripts/FlechaActivation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+
+public class FlechaActivation : StunActivation
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_navMeshController._is_selected 
+            && _navMeshController.GetComponent<PlayerController>().PlayerTurn == GameManager.Instance.JugadorActual
+            && Dado.Instance._numeroActual!=0)
+        {
+            _meshRenderer.enabled = true;
+           
+        }
+        else
+        {
+            _meshRenderer.enabled = false;
+        }
+    }
+}
--- Assets/_Scripts/StunActivation.cs
+++ Assets/_Scripts/StunActivation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(NavMeshController))]
+
 public class StunActivation : MonoBehaviour
 {
 
